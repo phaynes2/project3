a25923ec483dab92c609df5d7ae71c8c66071afe
phaynes2 <phaynes2@hawk.iit.edu>
2019-04-25 19:33:53 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,18 @@
-function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.844318, lng: -88.0768485},
-      zoom: 8
-});
+var map;
+var marker;
+var current;
+
+var chicago_il={lat: 41.844318, lng: -88.0768485}
+
+
+function initMap() {
+current={lat: 41.844318, lng: -88.0768485};
+        map = new google.maps.Map(document.getElementById('map'), {
+                zoom:15,
+        center: chicago_il});
+        marker = new google.maps.Marker({
+                position:chicago_il,
+                animation:google.maps.Animation.BOUNCE,
+        map: map });
 }
+
